c93400731230c23ab7d90752a57d4d827dae17e4
Daniela <dggtn9@gmail.com>
2024-02-21 19:40:59 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+canvas.width = 1024
+canvas.height = 576
+let y = 100
+function animate(){
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    c.fillStyle = 'red'
+    c.fillRect(0, 0, 200, 200)
+    y++
+}
+
+animate()
